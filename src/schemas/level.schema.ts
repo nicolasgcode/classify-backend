9cde7a15bb67cd4cb998449d3963eaadad0f395e
Joaquin Carlos Fernandez Da Silva <164550681+Joaquipe13@users.noreply.github.com>
2024-09-27 20:15:59 -0300

    Update level.schema.ts

--- src/schemas/level.schema.ts
+++ src/schemas/level.schema.ts
@@ -2,4 +2,13 @@ import { z } from 'zod';
 
 export const levelSchema = z.object({
   name: z.string().min(1, 'Name is required'),
+  course: z.number().int().positive().min(1, 'Course is required'),
 });
+export type LevelToPatch = z.infer<typeof LevelToPatchSchema>;
+export function validarLevelSchema(object:any) {
+    try {
+        return  LevelSchema.parse(object);
+    } catch (error:any) {  
+        throw error
+    }
+}
