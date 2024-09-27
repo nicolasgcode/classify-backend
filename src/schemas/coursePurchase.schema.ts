bad1d1119a87eb57d5cd4ba6aa383635007904da
Joaquin Carlos Fernandez Da Silva <164550681+Joaquipe13@users.noreply.github.com>
2024-09-27 20:12:14 -0300

    Update coursePurchase.schema.ts

--- src/schemas/coursePurchase.schema.ts
+++ src/schemas/coursePurchase.schema.ts
@@ -1,7 +1,16 @@
 import { z } from 'zod';
 
 export const coursePurchaseSchema = z.object({
-  course: z.number().min(1, 'Course id is required'),
-  member: z.number().min(1, 'Member id is required'),
-  totalAmount: z.number().min(1, 'totalAmount id is required'),
+  course: z.number().min(1, 'Course is required'),
+  member: z.number().min(1, 'Member is required'),
+  totalAmount: z.number().min(1, 'totalAmount is required'),
 });
+export type CoursePurchaseRecord = z.infer<typeof CoursePurchaseRecordSchema>;
+
+export function validarCoursePurchaseRecordSchema(object:any) {
+    try {
+        return  CoursePurchaseRecordSchema.parse(object);
+    } catch (error:any) {  
+        throw error
+    }
+}
